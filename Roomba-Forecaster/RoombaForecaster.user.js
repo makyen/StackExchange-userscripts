d7e14a3908af9c30a777d128a815db0d9810e6f0
Makyen <wp.makyen@gmail.com>
2017-03-11 20:48:50 -0800

    Roll revision to 2.0.0beta3

--- Roomba-Forecaster/RoombaForecaster.user.js
+++ Roomba-Forecaster/RoombaForecaster.user.js
@@ -2,7 +2,7 @@
 // @name         Roomba Forecaster
 // @author       Makyen
 // @author       Siguza
-// @version      2.0.0beta3.pre1
+// @version      2.0.0beta3
 // @description  Is Roomba going to delete the question? If not, why? If so, when?
 // @namespace    makyen-RoombaForecaster
 // @homepage     https://github.com/makyen/StackExchange-userscripts/tree/master/Roomba-Forecaster
