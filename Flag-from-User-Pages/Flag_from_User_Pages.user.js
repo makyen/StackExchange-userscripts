204bf279e179b8c56416c37dfc5b004555de9fee
double-beep <38133098+double-beep@users.noreply.github.com>
2021-07-18 09:25:49 +0000

    FIX: rename attribute data-postid to data-post-id
    See the getPostId() function in full.en.js: https://dev.stackoverflow.com/content/Js/full.en.js

--- Flag-from-User-Pages/Flag_from_User_Pages.user.js
+++ Flag-from-User-Pages/Flag_from_User_Pages.user.js
@@ -37,9 +37,9 @@
                         const matches = type === 'answer' ? href.match(/^.*#(\d+)$/) : href.match(/^\/q(?:uestions)?\/(\d+)\/.*$/);
                         if (matches && matches[1]) {
                             const postId = matches[1];
-                            $this.before('<a href="#" class="js-flag-post-link" title="flag this post for serious problems or moderator attention" data-postid="' + postId + '" style="margin-left:1em;margin-right:1em;display:inline;width:auto;">&#9873;</a>').css('display', 'inline');
+                            $this.before('<a href="#" class="js-flag-post-link" title="flag this post for serious problems or moderator attention" data-post-id="' + postId + '" style="margin-left:1em;margin-right:1em;display:inline;width:auto;">&#9873;</a>').css('display', 'inline');
                             $this.parent().find('.js-flag-post-link').data({
-                                postid: postId,
+                                'post-id': postId,
                                 loadedTimestamp: now,
                             });
                         }
